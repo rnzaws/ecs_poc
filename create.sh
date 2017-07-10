93c721d1bca1c2219e6f5b35d98da49ebc338f3b
Ryan <ryannitz@amazon.com>
2017-07-09 23:32:15 -0400

    Dev

--- create.sh
+++ create.sh
@@ -12,13 +12,19 @@ aws cloudformation create-stack --stack-name system-vpc-$APP_ENV --template-body
 
 aws cloudformation wait stack-create-complete --stack-name system-vpc-$APP_ENV
 
+aws cloudformation create-stack --stack-name system-bastion-$APP_ENV --template-body file://ops/cfn/bastion.cfn.yml \
+  --parameters ParameterKey=VpcStackName,ParameterValue=system-vpc-$APP_ENV
+
+aws cloudformation wait stack-create-complete --stack-name system-bastion-$APP_ENV
+
 aws cloudformation create-stack --stack-name app-alb-$APP_ENV --template-body file://ops/cfn/load-balancer.cfn.yml \
   --parameters ParameterKey=VpcStackName,ParameterValue=system-vpc-$APP_ENV
 
 aws cloudformation wait stack-create-complete --stack-name app-alb-$APP_ENV
 
 aws cloudformation create-stack --stack-name app-ecs-$APP_ENV --template-body file://ops/cfn/ecs-cluster.cfn.yml --capabilities CAPABILITY_NAMED_IAM \
-  --parameters ParameterKey=VpcStackName,ParameterValue=system-vpc-$APP_ENV ParameterKey=AlbStackName,ParameterValue=app-alb-$APP_ENV
+  --parameters ParameterKey=VpcStackName,ParameterValue=system-vpc-$APP_ENV ParameterKey=AlbStackName,ParameterValue=app-alb-$APP_ENV \
+    ParameterKey=BastionStackName,ParameterValue=system-bastion-$APP_ENV
 
 aws cloudformation wait stack-create-complete --stack-name app-ecs-$APP_ENV
 
